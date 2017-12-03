1d34fa33ce67721f26c5094ac2f7f03f9aa4c7da
Br1an6 <leaderrorme@gmail.com>
2017-12-03 15:40:14 -0600

    add code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CS-port-forwarding-for-MacOS
 Port-forwarding script for CS1.6, CZ on MacOS
+- Using homebrew with [MiniUPnP](http://miniupnp.tuxfamily.org)
+
+Simple Run:
+
+`./cs.sh`
+
+If there is an permisson error run (`sudo` if needed):
+
+`chmod +x cs.sh`
+
+
+License
+-------
+
+This software is licensed under the MIT license
+© 2017 CS port forwarding for MacOS contributor
---
+++ cs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author Br1an6
+# Visit: https://github.com/Br1an6/CS-port-forwarding-for-MacOS
+
+echo "[*] Check for homebrew"
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    echo "[*] Not found_Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else    
+	echo "[*] Found Homebrew and Updating"
+    brew update
+fi
+
+echo "[*] Check for upnpc"
+if [[ $(which upnpc) == "upnpc not found" ]] ; then
+    # Install Upnp client
+    echo "[*] Not found_Installing Upnp client"
+    brew install miniupnpc
+fi
+
+myip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+echo "[*] Start add rules to IP: $myip"
+upnpc -a $myip 27015 27015 TCP
+upnpc -a $myip 27015 27015 UDP
+echo "[*] Done"
