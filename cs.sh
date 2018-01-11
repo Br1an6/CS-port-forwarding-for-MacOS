d832e3370f67c6e540a5bfc2b8d0854330a0f333
Brian Yuan-An Liu <leaderrorme@gmail.com>
2018-01-11 06:28:00 -0600

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -9,8 +9,10 @@ if [[ $? != 0 ]] ; then
     echo "[*] Not found_Installing homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else    
-	echo "[*] Found Homebrew and Updating"
+    echo "[*] Found Homebrew and Updating"
     brew update
+    brew upgrade
+    brew clean
 fi
 
 echo "[*] Check for upnpc"
@@ -21,9 +23,14 @@ if [[ $(which upnpc) == "upnpc not found" ]] ; then
 fi
 
 myip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-echo "[*] Start adding rules to IP: $myip"
+echo "[*] Start adding rules with IP: $myip"
 # edit this part if needed
 # upnpc -a IP port external-port protocal
 upnpc -a $myip 27015 27015 TCP
 upnpc -a $myip 27015 27015 UDP
+
+# for removing rules uncommented the following two lines
+# upnpc -d 27015 TCP
+# upnpc -d 27015 UDP
+
 echo "[*] Done"
